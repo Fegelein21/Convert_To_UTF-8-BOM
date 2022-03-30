425af460f08de93079c90622e7b1f319c8f89c18
Fegelein21 <weixiaodongfm@foxmail.com>
2022-03-30 22:18:32 +0800

    upload

---
+++ ConvertToUTF8BOM/MainForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace ConvertToUTF8BOM
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SubFolderCheckBox = new System.Windows.Forms.CheckBox();
+			this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+			this.CopyFileCheckBox = new System.Windows.Forms.CheckBox();
+			this.FileExtTextBox = new System.Windows.Forms.TextBox();
+			this.label = new System.Windows.Forms.Label();
+			this.LogTextBox = new System.Windows.Forms.TextBox();
+			this.EncodingCheckedListBox = new System.Windows.Forms.CheckedListBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.EmptyFileCheckBox = new System.Windows.Forms.CheckBox();
+			this.AddEncodingTextBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// SubFolderCheckBox
+			// 
+			this.SubFolderCheckBox.AutoSize = true;
+			this.SubFolderCheckBox.Checked = true;
+			this.SubFolderCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.SubFolderCheckBox.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.SubFolderCheckBox.Location = new System.Drawing.Point(15, 14);
+			this.SubFolderCheckBox.Margin = new System.Windows.Forms.Padding(2);
+			this.SubFolderCheckBox.Name = "SubFolderCheckBox";
+			this.SubFolderCheckBox.Size = new System.Drawing.Size(110, 18);
+			this.SubFolderCheckBox.TabIndex = 0;
+			this.SubFolderCheckBox.Text = "处理子文件夹";
+			this.SubFolderCheckBox.UseVisualStyleBackColor = true;
+			// 
+			// backgroundWorker
+			// 
+			this.backgroundWorker.WorkerReportsProgress = true;
+			this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.WorkDealFiles);
+			this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+			this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+			// 
+			// CopyFileCheckBox
+			// 
+			this.CopyFileCheckBox.AutoSize = true;
+			this.CopyFileCheckBox.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.CopyFileCheckBox.Location = new System.Drawing.Point(15, 61);
+			this.CopyFileCheckBox.Name = "CopyFileCheckBox";
+			this.CopyFileCheckBox.Size = new System.Drawing.Size(152, 18);
+			this.CopyFileCheckBox.TabIndex = 2;
+			this.CopyFileCheckBox.Text = "复制无需处理的文件";
+			this.CopyFileCheckBox.UseVisualStyleBackColor = true;
+			// 
+			// FileExtTextBox
+			// 
+			this.FileExtTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.FileExtTextBox.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.FileExtTextBox.Location = new System.Drawing.Point(268, 12);
+			this.FileExtTextBox.Name = "FileExtTextBox";
+			this.FileExtTextBox.Size = new System.Drawing.Size(434, 23);
+			this.FileExtTextBox.TabIndex = 10;
+			this.FileExtTextBox.TextChanged += new System.EventHandler(this.FileExt_TextChanged);
+			// 
+			// label
+			// 
+			this.label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.label.AutoSize = true;
+			this.label.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label.Location = new System.Drawing.Point(199, 15);
+			this.label.Name = "label";
+			this.label.Size = new System.Drawing.Size(63, 14);
+			this.label.TabIndex = 12;
+			this.label.Text = "文件格式";
+			// 
+			// LogTextBox
+			// 
+			this.LogTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.LogTextBox.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.LogTextBox.Location = new System.Drawing.Point(202, 49);
+			this.LogTextBox.Multiline = true;
+			this.LogTextBox.Name = "LogTextBox";
+			this.LogTextBox.ReadOnly = true;
+			this.LogTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.LogTextBox.Size = new System.Drawing.Size(500, 300);
+			this.LogTextBox.TabIndex = 20;
+			this.LogTextBox.Text = "本工具用于将指定编码和格式的文件统统转换为UTF-8-BOM编码\r\n把需要处理的文件或文件夹拖入此窗口即可\r\n文件的输出名称为“原文件名称 (n)”\r\n文件夹的输" +
+    "出名称为“原文件夹名称 (Converted)”\r\n\r\n";
+			// 
+			// EncodingCheckedListBox
+			// 
+			this.EncodingCheckedListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.EncodingCheckedListBox.CheckOnClick = true;
+			this.EncodingCheckedListBox.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.EncodingCheckedListBox.FormattingEnabled = true;
+			this.EncodingCheckedListBox.HorizontalScrollbar = true;
+			this.EncodingCheckedListBox.Location = new System.Drawing.Point(15, 201);
+			this.EncodingCheckedListBox.Name = "EncodingCheckedListBox";
+			this.EncodingCheckedListBox.Size = new System.Drawing.Size(169, 148);
+			this.EncodingCheckedListBox.Sorted = true;
+			this.EncodingCheckedListBox.TabIndex = 11;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label1.Location = new System.Drawing.Point(12, 155);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(63, 14);
+			this.label1.TabIndex = 16;
+			this.label1.Text = "文件编码";
+			// 
+			// EmptyFileCheckBox
+			// 
+			this.EmptyFileCheckBox.AutoSize = true;
+			this.EmptyFileCheckBox.Checked = true;
+			this.EmptyFileCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.EmptyFileCheckBox.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.EmptyFileCheckBox.Location = new System.Drawing.Point(15, 37);
+			this.EmptyFileCheckBox.Name = "EmptyFileCheckBox";
+			this.EmptyFileCheckBox.Size = new System.Drawing.Size(96, 18);
+			this.EmptyFileCheckBox.TabIndex = 1;
+			this.EmptyFileCheckBox.Text = "处理空文件";
+			this.EmptyFileCheckBox.UseVisualStyleBackColor = true;
+			// 
+			// AddEncodingTextBox
+			// 
+			this.AddEncodingTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.AddEncodingTextBox.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.AddEncodingTextBox.ForeColor = System.Drawing.SystemColors.GrayText;
+			this.AddEncodingTextBox.Location = new System.Drawing.Point(15, 172);
+			this.AddEncodingTextBox.Name = "AddEncodingTextBox";
+			this.AddEncodingTextBox.Size = new System.Drawing.Size(169, 23);
+			this.AddEncodingTextBox.TabIndex = 12;
+			this.AddEncodingTextBox.Enter += new System.EventHandler(this.AddEncodingTextBox_Enter);
+			this.AddEncodingTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.AddEncodingTextBox_KeyPress);
+			this.AddEncodingTextBox.Leave += new System.EventHandler(this.AddEncodingTextBox_Leave);
+			// 
+			// MainForm
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.ClientSize = new System.Drawing.Size(714, 361);
+			this.Controls.Add(this.AddEncodingTextBox);
+			this.Controls.Add(this.EmptyFileCheckBox);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.EncodingCheckedListBox);
+			this.Controls.Add(this.LogTextBox);
+			this.Controls.Add(this.label);
+			this.Controls.Add(this.FileExtTextBox);
+			this.Controls.Add(this.CopyFileCheckBox);
+			this.Controls.Add(this.SubFolderCheckBox);
+			this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Margin = new System.Windows.Forms.Padding(2);
+			this.Name = "MainForm";
+			this.Text = "Convert To UTF-8-BOM";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.CheckBox SubFolderCheckBox;
+		private System.ComponentModel.BackgroundWorker backgroundWorker;
+		private System.Windows.Forms.CheckBox CopyFileCheckBox;
+		private System.Windows.Forms.TextBox FileExtTextBox;
+		private System.Windows.Forms.Label label;
+		private System.Windows.Forms.TextBox LogTextBox;
+		private System.Windows.Forms.CheckedListBox EncodingCheckedListBox;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.CheckBox EmptyFileCheckBox;
+		private System.Windows.Forms.TextBox AddEncodingTextBox;
+	}
+}
+
---
+++ ConvertToUTF8BOM/MainForm.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using UtfUnknown;
+
+namespace ConvertToUTF8BOM
+{
+	public partial class MainForm : Form
+	{
+		const string ConfigFile = "config.ini";
+		const string DefaultFileExt = ".csv .erb .erh .txt";
+		const string DefaultEncoding = "20127.1 65001.1 932.1 936.1 950.1";
+		static readonly Encoding UTF8BOM = new UTF8Encoding(true);
+
+		private string ConfigStr = "";
+
+		private string[] File_Ext;
+
+		private int TotalFileCount;
+		private int ConvertFileCount;
+		private int CopyFileCount;
+		private int ErrorFileCount;
+
+		public MainForm()
+		{
+			InitializeComponent();
+			ReadConfig();
+			AddEncodingTextBox_Leave1();
+		}
+
+		private void ReadConfig()
+		{
+			if (File.Exists(ConfigFile))
+			{
+				ConfigStr = File.ReadAllText(ConfigFile, Encoding.UTF8);
+			}
+
+			SubFolderCheckBox.Checked = GetConfig("HandleSubFolder", true);
+			EmptyFileCheckBox.Checked = GetConfig("HandleEmptyFile", true);
+			CopyFileCheckBox.Checked = GetConfig("CopyOtherFile", false);
+
+			FileExtTextBox.Text = GetConfig("FileExtension", DefaultFileExt).Trim();
+			Refresh_File_Ext();
+
+			string[] list = GetConfig("FileEncoding", DefaultEncoding).Trim().Split(' ');
+			for (int i = 0; i < list.Length; i++)
+			{
+				string[] data = list[i].Split('.');
+				data[0] = AddEncodingToList(data[0]);
+				EncodingCheckedListBox.SetItemChecked(EncodingCheckedListBox.Items.IndexOf(data[0]), data[1] == "1");
+			}
+		}
+
+		private void WriteConfig()
+		{
+			SetConfig("HandleSubFolder", SubFolderCheckBox.Checked.ToString());
+			SetConfig("HandleEmptyFile", EmptyFileCheckBox.Checked.ToString());
+			SetConfig("CopyOtherFile", CopyFileCheckBox.Checked.ToString());
+			SetConfig("FileExtension", FileExtTextBox.Text);
+
+			string[] data = new string[EncodingCheckedListBox.Items.Count];
+			EncodingCheckedListBox.Items.CopyTo(data, 0);
+			for (int i = 0; i < EncodingCheckedListBox.Items.Count; i++)
+			{
+				data[i] += "." + (EncodingCheckedListBox.GetItemChecked(i) ? "1" : "0");
+			}
+			SetConfig("FileEncoding", string.Join(" ", data));
+
+			File.WriteAllText(ConfigFile, ConfigStr, Encoding.UTF8);
+		}
+
+		private string GetConfig(string key, string defaultVar = "")
+		{
+			if (ConfigStr == "")
+			{
+				return defaultVar;
+			}
+			Match match = Regex.Match(ConfigStr, string.Format("{0}=(.*)", key));
+			return match.Groups.Count > 1 ? match.Groups[1].ToString() : defaultVar;
+		}
+
+		private bool GetConfig(string key, bool defaultVar)
+		{
+			return bool.TryParse(GetConfig(key), out bool result) ? result : defaultVar;
+		}
+
+		private void SetConfig(string key, string value)
+		{
+			string format = string.Format("{0}=.*", key);
+			if (Regex.Matches(ConfigStr, format).Count > 0)
+			{
+				ConfigStr = Regex.Replace(ConfigStr, format, key + "=" + value);
+			}
+			else
+			{
+				ConfigStr += key + "=" + value + "\r\n";
+			}
+		}
+
+		private string GetEnableFilename(string path)
+		{
+			if (!File.Exists(path) && !Directory.Exists(path))
+			{
+				return path;
+			}
+
+			int rename = 1;
+			string dict = Path.GetDirectoryName(path);
+			string name = Path.GetFileNameWithoutExtension(path);
+			string ext = Path.GetExtension(path);
+
+			while (File.Exists(path) || Directory.Exists(path))
+			{
+				path = string.Format("{0}\\{1} ({2}){3}", dict, name, rename++, ext);
+			}
+
+			return path;
+		}
+
+		private Encoding DetectEncoding(string path)
+		{
+
+			DetectionResult result = CharsetDetector.DetectFromFile(path);
+			if (result.Detected == null)
+			{
+				if (File.ReadAllText(path, Encoding.UTF8) == "")
+				{
+					return EmptyFileCheckBox.Checked ? UTF8BOM : null;
+				}
+				DebugLog("无法识别编码", path);
+				return null;
+			}
+
+			Encoding encoding = result.Detected.Encoding;
+			int index = EncodingCheckedListBox.Items.IndexOf(encoding.WebName);
+
+			if (index >= 0)
+			{
+				bool encodingCheck = EncodingCheckedListBox.GetItemChecked(index);
+				if (encodingCheck && encoding.CodePage == UTF8BOM.CodePage && result.Detected.HasBOM)
+				{
+					encodingCheck = false;
+				}
+
+				return encodingCheck ? encoding : null;
+			}
+
+			DebugLog("预期外的编码", string.Format("[{0}] {1}", result.Detected.Encoding.WebName, path));
+			return null;
+		}
+
+		private void DealOneFile(string path, string topDict)
+		{
+			TotalFileCount++;
+			string fileName = Path.GetFileName(path);
+			string out_path = GetEnableFilename(topDict + "\\" + fileName);
+			//DebugLog("dealOneFile", out_path);
+
+			try
+			{
+				if (File_Ext.Contains(Path.GetExtension(path).ToLower()))
+				{
+					Encoding encoding = DetectEncoding(path);
+					if (encoding != null)
+					{
+						File.WriteAllText(out_path, File.ReadAllText(path, encoding), UTF8BOM);
+						ConvertFileCount++;
+						return;
+					}
+				}
+
+				if (CopyFileCheckBox.Checked)
+				{
+					File.Copy(path, out_path);
+					CopyFileCount++;
+				}
+			}
+			catch (Exception e)
+			{
+				DebugLog("未知错误", e.Message + " : " + path);
+				ErrorFileCount++;
+			}
+		}
+
+		private void DealFiles(string filePath, string topDict)
+		{
+			if (!Directory.Exists(topDict))
+			{
+				Directory.CreateDirectory(topDict);
+			}
+
+			foreach (var path in Directory.GetFiles(filePath))
+			{
+				DealOneFile(path, topDict);
+			}
+
+			backgroundWorker.ReportProgress(0);
+
+			if (SubFolderCheckBox.Checked)
+			{
+				foreach (var path in Directory.GetDirectories(filePath))
+				{
+					DealFiles(path, topDict + "\\" + Path.GetFileName(path));
+				}
+			}
+		}
+
+		private void WorkDealFiles(object sender, DoWorkEventArgs e)
+		{
+			string[] data = (string[])e.Argument;
+
+			foreach (var path in data)
+			{
+				if (Directory.Exists(path))
+				{
+					string topDict = path + " (Converted)";
+					if (!Directory.Exists(topDict))
+					{
+						Directory.CreateDirectory(topDict);
+					}
+					DealFiles(path, topDict);
+				}
+				else
+				{
+					DealOneFile(path, Path.GetDirectoryName(path));
+				}
+			}
+		}
+
+		private void MainForm_DragDrop(object sender, DragEventArgs e)
+		{
+			try
+			{
+				string[] data = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+				if (data.Length > 0 && EncodingCheckedListBox.CheckedItems.Count > 0 && File_Ext.Length > 0)
+				{
+					AllowDrop = false;
+					TotalFileCount = 0;
+					ConvertFileCount = 0;
+					CopyFileCount = 0;
+					ErrorFileCount = 0;
+
+					AddTextLog("开始处理，请稍等。。。");
+					backgroundWorker.RunWorkerAsync(data);
+				}
+			}
+			catch (Exception ex)
+			{
+				AllowDrop = true;
+				AddTextLog(ex.ToString());
+			}
+		}
+
+		private void MainForm_DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				e.Effect = DragDropEffects.All;
+			}
+			else
+			{
+				e.Effect = DragDropEffects.None;
+			}
+		}
+
+		private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+		{
+			if (e.UserState != null)
+			{
+				string text = e.UserState.ToString();
+				if (text != "")
+				{
+					AddTextLog(text);
+				}
+			}
+		}
+
+		private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			AllowDrop = true;
+			AddTextLog(string.Format("转码完成: 共计{0}个文件，{1}个文件转换成功，{2}个文件仅复制，{3}个错误。", TotalFileCount, ConvertFileCount, CopyFileCount, ErrorFileCount));
+		}
+
+		private void DebugLog(string tag, string text)
+		{
+			backgroundWorker.ReportProgress(0, tag + ": " + text);
+		}
+
+		private void AddTextLog(string text)
+		{
+			LogTextBox.Text += text + "\r\n";
+			LogTextBox.Select(LogTextBox.Text.Length, 0);
+			LogTextBox.ScrollToCaret();
+		}
+
+		private void FileExt_TextChanged(object sender, EventArgs e)
+		{
+			Refresh_File_Ext();
+		}
+
+		private void Refresh_File_Ext()
+		{
+			if (FileExtTextBox.Text.Trim() == "")
+			{
+				File_Ext = new string[0];
+			}
+			else
+			{
+				File_Ext = FileExtTextBox.Text.Trim().ToLower().Split(' ');
+			}
+		}
+
+		private void AddEncodingTextBox_Enter(object sender, EventArgs e)
+		{
+			AddEncodingTextBox.Text = "";
+			AddEncodingTextBox.ForeColor = System.Drawing.Color.Black;
+		}
+
+		private void AddEncodingTextBox_Leave(object sender, EventArgs e)
+		{
+			AddEncodingTextBox_Leave1();
+		}
+
+		private void AddEncodingTextBox_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (e.KeyChar == ((char)Keys.Enter))
+			{
+				AddEncodingToList(AddEncodingTextBox.Text);
+				EncodingCheckedListBox.Select();
+			}
+		}
+
+		private void AddEncodingTextBox_Leave1()
+		{
+			AddEncodingTextBox.Text = "添加编码(名称或代码页)";
+			AddEncodingTextBox.ForeColor = System.Drawing.Color.Gray;
+		}
+
+		private string AddEncodingToList(string text)
+		{
+			Encoding encoding = GetEncodingByStrCodePage(text);
+			if (encoding != null)
+			{
+				if (!EncodingCheckedListBox.Items.Contains(encoding.WebName))
+					EncodingCheckedListBox.Items.Add(encoding.WebName);
+				return encoding.WebName;
+			}
+			return "";
+		}
+
+		private Encoding GetEncodingByStrCodePage(string text)
+		{
+			try
+			{
+				if (int.TryParse(text, out int codePage))
+					return Encoding.GetEncoding(codePage);
+				else
+					return Encoding.GetEncoding(text);
+			}
+			catch
+			{
+			}
+			return null;
+		}
+
+		private void MainForm_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+		{
+			WriteConfig();
+		}
+	}
+}
